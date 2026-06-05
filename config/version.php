d0f74b8d33e5b16f52042c7a88b54703b2b59b38
akhr0m <ahmadakhromi250294@gmail.com>
2026-06-05 15:57:11 +0700

    Fix: resolve git merge conflict markers in config/version.php

--- config/version.php
+++ config/version.php
@@ -4,13 +4,7 @@ return array (
   'full_app_version' => 'v8.1.18 - build 18876-g4c6249eb9',
   'build_version' => '18876',
   'prerelease_version' => '',
-<<<<<<< HEAD
-  'hash_version' => 'g0f92dee2c',
-  'full_hash' => 'v8.1.16-220-g0f92dee2c',
-  'branch' => 'master',
-=======
   'hash_version' => 'g4c6249eb9',
   'full_hash' => 'v8.1.18-77-g4c6249eb9',
   'branch' => 'develop',
->>>>>>> upstream/develop
 );
