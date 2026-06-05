d9fcec5ed97a42646f5cbcee5213a250e69ef34c
akhr0m <ahmadakhromi250294@gmail.com>
2026-06-05 17:32:34 +0700

    feat: integrate automated SemVer and GitHub Actions release workflow

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v8.1.18',
-  'full_app_version' => 'v8.1.18 - build 18876-g4c6249eb9',
+  'app_version' => 'v8.1.18-bast.1.0.0',
+  'full_app_version' => 'v8.1.18-bast.1.0.0 - build 18876-g8160c697f2',
   'build_version' => '18876',
   'prerelease_version' => '',
-  'hash_version' => 'g4c6249eb9',
-  'full_hash' => 'v8.1.18-77-g4c6249eb9',
-  'branch' => 'develop',
+  'hash_version' => 'g8160c697f2',
+  'full_hash' => 'v8.1.18-bast.1.0.0-g8160c697f2',
+  'branch' => 'feature/custom-bast-module',
 );
